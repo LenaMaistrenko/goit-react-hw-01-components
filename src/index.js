ff13af000ecc9059ffb7581bc5d3a1d59f31a59d
LenaMaistrenko <istlauder@gmail.com>
2023-02-08 02:04:45 +0200

    +

--- src/index.js
+++ src/index.js
@@ -8,26 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
-{
-  /* <li className={css.item}>
-      <span className={isOnline ? css.online: css.offline}>{isOnline}</span>
-      <img class="avatar" src={avatar} alt="User avatar" width="48" />
-      <p class="name">{name}</p>
-    </li>
-.status {
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-}
-
-.online {
-  composes: status;
-  background-color: yellowgreen;
-}
-
-.offline {
-  composes: status;
-  background-color: red;
-} */
-}
